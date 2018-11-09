4588a65723fe1ee8a115ae28c06a09efeb5d730d
Roger Perez <rperezga@gmail.com>
2018-11-08 21:08:40 -0500

    First commit

---
+++ bamazonCustomer.js
@@ -0,0 +1,24 @@
+var mysql = require("mysql");
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "root",
+  database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  afterConnection();
+});
+
+function afterConnection() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    connection.end();
+  });
+}
